67b625164d71b566cb466b864450bb450138b904
Vijayalaxmi Shirnal <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2024-06-05 23:21:17 +0530

    Add files via upload

---
+++ portfolio/Portfolio/README.md
@@ -0,0 +1,3 @@
+##  Hi, this is my portfolio 
+
+### I created it using skills such as HTML, CSS, and JavaScript frequently
---
+++ portfolio/Portfolio/script.js
@@ -0,0 +1,128 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//scroll sections
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+           // active navbar links
+           navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           });
+           //active section for animation scroll
+           sec.classList.add('show-animate')
+        }
+        // if want to use animation scroll that repets on scroll
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    //anmation footer scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
+// animation congrats
+
+const Confettiful = function(el) {
+    this.el = el;
+    this.containerEl = null;
+    
+    this.confettiFrequency = 3;
+    this.confettiColors = ['#EF2964', '#00C09D', '#2D87B0', '#48485E','#EFFF1D'];
+    this.confettiAnimations = ['slow', 'medium', 'fast'];
+    
+    this._setupElements();
+    this._renderConfetti();
+  };
+  
+  Confettiful.prototype._setupElements = function() {
+    const containerEl = document.createElement('div');
+    const elPosition = this.el.style.position;
+    
+    if (elPosition !== 'relative' || elPosition !== 'absolute') {
+      this.el.style.position = 'relative';
+    }
+    
+    containerEl.classList.add('confetti-container');
+    
+    this.el.appendChild(containerEl);
+    
+    this.containerEl = containerEl;
+  };
+  
+  Confettiful.prototype._renderConfetti = function() {
+    this.confettiInterval = setInterval(() => {
+      const confettiEl = document.createElement('div');
+      const confettiSize = (Math.floor(Math.random() * 3) + 7) + 'px';
+      const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
+      const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
+      const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
+      
+      confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+      confettiEl.style.left = confettiLeft;
+      confettiEl.style.width = confettiSize;
+      confettiEl.style.height = confettiSize;
+      confettiEl.style.backgroundColor = confettiBackground;
+      
+      confettiEl.removeTimeout = setTimeout(function() {
+        confettiEl.parentNode.removeChild(confettiEl);
+      }, 3000);
+      
+      this.containerEl.appendChild(confettiEl);
+    }, 25);
+  };
+  
+  
+  function start(){
+    new Confettiful(document.querySelector('.js-container'));
+    let congratEl = document.querySelector('.congrats');
+    congratEl.style.display = 'block';
+  }
+  
+  
+  let test = document.querySelector('.recruter');
+  test.addEventListener('click', start );
+
+  //button endanim
+  
+  const endanim = document.querySelector('#endanim');
+  endanim.addEventListener('click',() => {
+    const el = document.querySelector('.congrats');
+  const congratsEl = document.querySelector('.confetti-container');
+    congratsEl.style.display = 'none';
+    el.style.display = 'none';
+  });
+  
+
+  
+  
+
+  
+  
+  
